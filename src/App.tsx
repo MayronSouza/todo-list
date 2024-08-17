8d8c16152259c95c31209d9226bdbbfde3d645ec
MayronSouza <mayron.souza@gmz.solutions>
2024-08-17 10:54:42 -0300

    create function to toggle between the tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid'
 import { PlusCircle } from "@phosphor-icons/react"
 
@@ -5,11 +6,10 @@ import { Header } from "./components/Header"
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
 import { Header as ListHeader } from './components/List/Header'
+import { Item } from "./components/List/Item"
+import { Empty } from './components/List/Empty'
 
 import styles from "./App.module.css"
-import { Item } from "./components/List/Item"
-import { useState } from "react"
-import { Empty } from './components/List/Empty'
 
 export interface ITasks {
   id: string,
@@ -54,6 +54,18 @@ function App() {
     setTasks(filteredTasks)
   }
 
+  function handleToggleTask({ id, value }: { id: string, value: boolean }) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isChecked: value }
+      }
+
+      return { ...task }
+    })
+
+    setTasks(updatedTasks)
+  }
+
   return (
     <main>
       <Header />
@@ -81,6 +93,7 @@ function App() {
                 key={task.id}
                 data={task}
                 removeTask={handleRemoveTask}
+                toggleTaskStatus={handleToggleTask}
               />
             ))}
           </div>
