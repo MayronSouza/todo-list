c0241457f9dde4b2d3827b237c0d9113eaae9a4f
MayronSouza <mayron.souza@gmz.solutions>
2024-08-17 00:35:02 -0300

    create a task add function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import { PlusCircle } from "@phosphor-icons/react"
 
 import { Header } from "./components/Header"
@@ -7,16 +8,44 @@ import { Header as ListHeader } from './components/List/Header'
 
 import styles from "./App.module.css"
 import { Item } from "./components/List/Item"
-// import { Empty } from "./components/List/Empty"
+import { useState } from "react"
+import { Empty } from './components/List/Empty'
+
+export interface ITasks {
+  id: string,
+  text: string,
+  isChecked: boolean,
+}
 
 function App() {
+  const [tasks, setTasks] = useState<ITasks[]>([])
+  const [inputValue, setInputValue] = useState('')
+
+  function handleAddTask() {
+    if (!inputValue) {
+      return
+    }
+
+    const newTask: ITasks = {
+      id: uuidv4(),
+      text: inputValue,
+      isChecked: false,
+    }
+
+    setTasks((state) => [...state, newTask])
+    setInputValue('')
+  }
+
   return (
     <main>
       <Header />
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
-          <Input />
-          <Button>
+          <Input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+          />
+          <Button onClick={handleAddTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2"/>
           </Button>
@@ -24,12 +53,18 @@ function App() {
         <div className={styles.taskList}>
           <ListHeader />
         </div>
-        <div>
-          <Item />
-          <Item />
-          <Item />
-          {/* <Empty /> */}
-        </div>
+        {tasks.length > 0 ? (
+          <div>
+            {tasks.map((task) => (
+              <Item
+                key={task.id}
+                data={task}
+              />
+            ))}
+          </div>
+        ) : (
+          <Empty />
+        )}
       </section>
     </main>
   )
--- src/components/List/Item.tsx
+++ src/components/List/Item.tsx
@@ -1,16 +1,21 @@
-import { Trash } from '@phosphor-icons/react'
+import { Check, Trash } from '@phosphor-icons/react'
 import styles from './Item.module.css'
+import { ITasks } from '../../App'
 
-export function Item() {
+interface ItemProps {
+  data: ITasks
+}
+
+export function Item({ data }: ItemProps) {
   return (
     <div className={styles.container}>
       <div>
         <label htmlFor="checkbox">
-          <input readOnly type="checkbox" />
+          <input readOnly type="checkbox" checked={data.isChecked} />
           <span>
-
+            {data.isChecked && <Check size={12} />}
           </span>
-          <p>Desenvolver uma SPA com ReactJS</p>
+          <p>{data.text}</p>
         </label>
       </div>
       
